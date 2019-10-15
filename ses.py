1d24eaf9c3636dd90558f787b52e6572f3304b82
RajNikhil Marpu <rajmarpu575@gmail.com>
2019-10-15 18:30:16 +0530

    Changes

--- ses.py
+++ ses.py
@@ -8,17 +8,17 @@ client = boto3.client('ses')
 
 message = MIMEMultipart()
 message['Subject'] = 'From API ---- Sublime Text'
-message['From'] = 'rajnikhil@clouber.io'
-message['To'] = ', '.join(['rajnikhil@clouber.io','rajmarpu575@gmail.com'])
+message['From'] = 'XXXXXXXXXXXXXXXXXXXXXx'
+message['To'] = ', '.join(['XXXXXXXXXXXXXXXXXXXXX','XXXXXXXXXXXXXXXXXXXXXXX'])
 # message body
 part = MIMEText('email body string', 'html')
 message.attach(part)
 # attachment
-attachment_string = 'raj.txt'
+attachment_string = 'ses.txt'
 if attachment_string:   # if bytestring available
-    part = MIMEApplication(str.encode('Hai, This is Raj from AWS SES, I am sending you a mail from Sydney Region'))
+    part = MIMEApplication(str.encode('Hai, This is BOTO3 from AWS SES, I am sending you a mail '))
 else:    # if file provided
     part = MIMEApplication(open(raj.txt, 'rb').read())
 part.add_header('Content-Disposition', 'attachment', filename='raj.txt')
 message.attach(part)
-response = client.send_raw_email(Source=message['From'],Destinations=['rajnikhil@clouber.io', 'rajmarpu575@gmail.com'],RawMessage={'Data': message.as_string()})
+response = client.send_raw_email(Source=message['From'],Destinations=['XXXXXXXXXXXXXXXXX', 'XXXXXXXXXXXXXXXXXXXXXX'],RawMessage={'Data': message.as_string()})
--- writingtodynamodb.py
+++ writingtodynamodb.py
@@ -1,4 +1,4 @@
 import boto3
 dynamodb = boto3.client('dynamodb',region_name='us-east-1')
 
-dynamodb.put_item(TableName='customerInfo', Item= {'Name': {'S':'AshokRajan'},'Phone Number': {'N':'+17034660228'}})
+dynamodb.put_item(TableName='customerInfo', Item= {'Name': {'S':'XXXXXXXXXX'},'Phone Number': {'N':'XXXXXXXXXX'}})
