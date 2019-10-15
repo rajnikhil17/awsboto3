eeca087947839cb6723331f4566c095c9c4db65d
rajnikhil17 <rajmarpu575@gmail.com>
2019-10-15 17:58:33 +0530

    Delete release_elastic_ip's.py

--- release_elastic_ip's.py
+++
@@ -1,8 +0,0 @@
-import boto3
-client = boto3.client('ec2')
-addresses_dict = client.describe_addresses()
-print addresses_dict
-for eip_dict in addresses_dict['Addresses']:
-	if "InstanceId" not in eip_dict:
-		print (eip_dict['PublicIp'] +" doesn't have any instances associated, releasing")
-        client.release_address(AllocationId=eip_dict['AllocationId'])
